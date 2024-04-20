0cfc334173d930b3857aa5f50d98071620b2165d
Dave Russell <dave.aj.russell@gmail.com>
2024-04-20 20:51:15 +0100

    fix import path

--- apps/budgie-api/src/routes/entriesRoutes.ts
+++ apps/budgie-api/src/routes/entriesRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import entriesController from '../controllers/entryController';
+import entriesController from '../controllers/entriesController';
 
 const entriesApi = express.Router();
 
